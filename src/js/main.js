a682970e587bc0de7ac55159f950b0022ffdc96a
Cidrallll <lucas.woginiack@yahoo.com.br>
2022-06-01 16:37:43 -0300

    Select year com os anos

--- src/js/main.js
+++ src/js/main.js
@@ -23,6 +23,23 @@ document.querySelector('.steps').addEventListener('click', (event) => {
 
 })
 
+// Listando os anos do Select 
+
+
+/*
+let listAge = [];
+for(let i = 1903; i < 2023; i++) {
+    listAge[i] = i;
+}  
+listAge.sort((a, b) => b - a);
+
+const yearId = document.getElementById("year");
+const years = listAge;
+years.forEach((age) => {
+    option = new Option(age);
+    yearId.options[yearId.options.length] = option;
+});*/
+
 //Ao clicar no botão enviar (realizar validações e trocar de tela)
 document.querySelector('#submit-form').addEventListener('click', (event) => {
     event.preventDefault()
