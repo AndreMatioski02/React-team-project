cb9ebfa07fb12e986b7a714578b6eebd9bfe3f0b
Cidrallll <lucas.woginiack@yahoo.com.br>
2022-06-01 22:18:02 -0300

    Input year ok / local storage ok

--- src/js/main.js
+++ src/js/main.js
@@ -26,23 +26,6 @@ document.querySelector('.steps').addEventListener('click', (event) => {
 */
 })
 
-// Listando os anos do Select 
-
-
-/*
-let listAge = [];
-for(let i = 1903; i < 2023; i++) {
-    listAge[i] = i;
-}  
-listAge.sort((a, b) => b - a);
-
-const yearId = document.getElementById("year");
-const years = listAge;
-years.forEach((age) => {
-    option = new Option(age);
-    yearId.options[yearId.options.length] = option;
-});*/
-
 //Ao clicar no botão enviar (realizar validações e trocar de tela)
 //para funcionar é necessario que exista um formulário na tela com um data-page='id_da_pg_seguinte'
 dynamicContent.addEventListener('submit', (event) => {
