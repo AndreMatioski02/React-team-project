b91ce190401a3e679a4b4d11c73d638ed6bff5ba
victor.paula.pb <victor.paula.pb@compasso.com.br>
2022-06-03 13:52:14 -0300

    Mostrando certificados na tela de listagem, mostrando com base nos favoritos

--- src/js/view/list-view.js
+++ src/js/view/list-view.js
@@ -1,31 +1,39 @@
-class ListView{
-    static list(){
-        let data = JSON.parse(localStorage.getItem(`users`));
-        const div = document.querySelector('.content');
-        const card = document.createElement("div");
-        
-        card.style.display='flex';
+class ListView {
+    static list() {
+            let data = JSON.parse(localStorage.getItem(`users`));
+            const div = document.querySelector('.content');
+            const card = document.createElement("div");
+            const certi = document.createElement('div');
 
-        data.forEach(item => { 
-            card.innerHTML =  card.innerHTML + `<div class="list-div">
-                <h1 class="title-card">${item.fullName}</h1>     
-                <h3 class="subtitle-list">Nickname: ${item.nickname}</h3>
-                <h3 class="subtitle-list">Email: ${item.email}</h3>
-                <h3 class="subtitle-list">Phone:${item.phone}}</h3>
-                <h3 class="subtitle-list">Date: ${item.day}/${item.month}/${item.year}</h3><br>       
-                
-                <h3 class="subtitle-list">Github: ${item.github}</h3>
-                <h3 class="subtitle-list">Linkedin: ${item.linkedin}</h3><br>
+            card.style.display = 'flex';
 
-                    ${item.certificates.forEach((certificate)=>{
-                        // console.log(certificate);
-                        const certi = document.createElement('div');
-                        
-                        certi.innerHTML =certi.innerHTML+`<h3 class="subtitle-list">Certificates:${certificate}</h3>`;
-                        
-                        console.log(certi)
-                    })};
-                </div>
+            data.forEach(item => {
+                        card.innerHTML = card.innerHTML + `
+                <div class="list-div">
+                    <h1 class="title-card">${item.fullName}</h1>     
+                    <h3 class="subtitle-list">Nickname: ${item.nickname}</h3>
+                    <h3 class="subtitle-list">Email: ${item.email}</h3>
+                    <h3 class="subtitle-list">Phone:${item.phone}}</h3>
+                    <h3 class="subtitle-list">Date: ${item.day}/${item.month}/${item.year}</h3><br>       
+                    
+                    <h3 class="subtitle-list">Github: ${item.github}</h3>
+                    <h3 class="subtitle-list">Linkedin: ${item.linkedin}</h3><br>
+                        ${
+                            item.certificates.map((certificate, index)=>{
+                            // console.log(certificate);
+                            //certi.innerHTML =certi.innerHTML+`<h3 class="subtitle-list">Certificates:${certificate}</h3>`;
+                            if(item.favorites[index] == 'true')
+                                return `<h3 class="subtitle-list">Certificates:${certificate}</h3>`;
+                            }).join(``)
+                        }
+                        ${
+                            item.certificates.map((certificate, index)=>{
+                            // console.log(certificate);
+                            //certi.innerHTML =certi.innerHTML+`<h3 class="subtitle-list">Certificates:${certificate}</h3>`;
+                            if(item.favorites[index] == 'false' || item.favorites[index] == null)
+                                return `<h3 class="subtitle-list">Certificates: ${certificate}</h3>`;
+                            }).join(``)
+                        }
 
                 </div>`;
         });
